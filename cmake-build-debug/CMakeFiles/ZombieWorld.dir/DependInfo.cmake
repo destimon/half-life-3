0e7d3fbaf280ccff82bc742970f81aa65f57454f
destimon <destaom@gmail.com>
2018-10-20 00:32:14 +0300

    nvm

---
+++ cmake-build-debug/CMakeFiles/ZombieWorld.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Clion/ZombieWorld/main.cpp" "D:/Clion/ZombieWorld/cmake-build-debug/CMakeFiles/ZombieWorld.dir/main.cpp.obj"
+  "D:/Clion/ZombieWorld/src/Ally.cpp" "D:/Clion/ZombieWorld/cmake-build-debug/CMakeFiles/ZombieWorld.dir/src/Ally.cpp.obj"
+  "D:/Clion/ZombieWorld/src/Enemy.cpp" "D:/Clion/ZombieWorld/cmake-build-debug/CMakeFiles/ZombieWorld.dir/src/Enemy.cpp.obj"
+  "D:/Clion/ZombieWorld/src/Entity.cpp" "D:/Clion/ZombieWorld/cmake-build-debug/CMakeFiles/ZombieWorld.dir/src/Entity.cpp.obj"
+  "D:/Clion/ZombieWorld/src/Game.cpp" "D:/Clion/ZombieWorld/cmake-build-debug/CMakeFiles/ZombieWorld.dir/src/Game.cpp.obj"
+  "D:/Clion/ZombieWorld/src/Player.cpp" "D:/Clion/ZombieWorld/cmake-build-debug/CMakeFiles/ZombieWorld.dir/src/Player.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ZombieWorld.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZombieWorld.dir/main.cpp.obj"
+  "CMakeFiles/ZombieWorld.dir/src/Entity.cpp.obj"
+  "CMakeFiles/ZombieWorld.dir/src/Enemy.cpp.obj"
+  "CMakeFiles/ZombieWorld.dir/src/Player.cpp.obj"
+  "CMakeFiles/ZombieWorld.dir/src/Game.cpp.obj"
+  "CMakeFiles/ZombieWorld.dir/src/Ally.cpp.obj"
+  "ZombieWorld.pdb"
+  "ZombieWorld.exe"
+  "ZombieWorld.exe.manifest"
+  "libZombieWorld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZombieWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
